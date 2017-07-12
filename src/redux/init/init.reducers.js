06f3b1cd416533d22bc07640afd4279b17cbcc06
DuyNguyen <votheduy154@gmail.com>
2017-07-12 09:01:45 +0700

    no message

--- src/redux/init/init.reducers.js
+++ src/redux/init/init.reducers.js
@@ -3,13 +3,13 @@ import * as types from './init.types'
 const initialState = {}
 
 export default function init(state = initialState, action) {
-    switch (action.type) {
-        case types.INIT_APPLICATION:
-            return {
-                ...action.payload,
-                reduxConnect: true
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case types.INIT_APPLICATION:
+      return {
+        ...action.payload,
+        reduxConnect: true
+      }
+    default:
+      return state
+  }
 }
