9fab6bc1f67b3f0087889cf9b774b42cf05af9aa
DuyNguyen <votheduy154@gmail.com>
2017-07-12 09:03:19 +0700

    config store, reducer, fonts ...

--- src/configs/reducer.config.js
+++ src/configs/reducer.config.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
-import { initReducer } from '../redux'
+import {combineReducers} from 'redux'
+import {routerReducer} from 'react-router-redux'
+import {initReducer} from '../redux'
 
 const rootReducer = combineReducers({
-    initStore: initReducer,
-    routing: routerReducer,
+  initStore: initReducer,
+  routing: routerReducer,
 })
 export default rootReducer
 
